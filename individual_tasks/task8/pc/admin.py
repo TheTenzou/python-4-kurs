ee88e32c01f1a81408908bb472564ed87045999b
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-03-06 11:43:17 +0300

    Task8 add models

--- individual_tasks/task8/pc/admin.py
+++ individual_tasks/task8/pc/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Computer, Cpu, Gpu
 # Register your models here.
+
+admin.site.register(Computer)
+admin.site.register(Cpu)
+admin.site.register(Gpu)
--- individual_tasks/task8/pc/models.py
+++ individual_tasks/task8/pc/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Cpu(models.Model):
+    name = models.CharField(max_length=30)
+    core_count = models.IntegerField()
+    core_clock = models.FloatField()
+    _hash = models.IntegerField()
+
+    def __unicode__(self):
+        return self.name
+
+
+class Gpu(models.Model):
+    name = models.CharField(max_length=30)
+    core_count = models.IntegerField()
+    core_clock = models.FloatField()
+    vram = models.IntegerField()
+
+    def __unicode__(self):
+        return self.name
+
+
+class Computer(models.Model):
+    name = models.CharField(max_length=30)
+    cpu = models.ForeignKey(Cpu, on_delete=models.CASCADE)
+    gpu = models.ForeignKey(Gpu, on_delete=models.CASCADE)
+
+    def __unicode__(self):
+        return self.name
